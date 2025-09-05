2f4ca397a8b9ba1f2cb53204c37cc79e1bf94adb
marcelovicentegc <marcelovicentegc@gmail.com>
2025-09-05 22:17:28 +0200

    chore: capture user id from headers

--- lib/middleware/auth.ts
+++ lib/middleware/auth.ts
@@ -3,8 +3,14 @@ import { Request, Response, NextFunction } from "express";
 // This is a placeholder for a real authentication middleware.
 // In a production application, you would use a library like Passport.js
 // to handle user authentication and protect your routes.
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const { userId } = req.body.userId ? req.body : req.query;
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let { userId } = req.body.userId ? req.body : req.query;
+
+  userId = userId || req.headers["X-User-Id"] || req.headers["x-user-id"];
 
   if (!userId) {
     return res.status(401).json({
@@ -12,9 +18,6 @@ export const authenticate = (req: Request, res: Response, next: NextFunction) =>
       error: "Unauthorized",
     });
   }
-
-  // In a real application, you would validate the userId against a database
-  // or a session to ensure the user is authenticated.
 
   next();
 };
